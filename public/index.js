83dd8c482d30510a1e6626976fca098de21ee8cb
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-19 01:53:24 -0700

    header and image

--- public/index.js
+++ public/index.js
@@ -1,1 +1,22 @@
 // Your code here
+
+window.onload = async () => {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Kitten Pic';
+  addToDom(h1);
+  const img = document.createElement('img');
+  img.src = await getCat();
+  addToDom(img);
+};
+
+function addToDom(element) {
+  document.body.appendChild(element);
+}
+
+async function getCat() {
+  const randomCatUrl = 'https://api.thecatapi.com/v1/images/search';
+
+  const cat = await fetch(randomCatUrl);
+  const res = await cat.json();
+  return res[0]['url'];
+}
